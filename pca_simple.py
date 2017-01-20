ab88f59bf2f00c41e7d925344dbbdd6ee66e98c8
kcotar <cotar.klemen@gmail.com>
2017-01-20 13:15:53 +0100

    Daily commit

--- pca_simple.py
+++ pca_simple.py
@@ -153,34 +153,36 @@ use_cols = get_abundance_cols(cannon_data.colnames)
 
 # abund_data = np.array(cannon_data_norm[use_cols].to_pandas())
 abund_data = np.array(cannon_data[use_cols].to_pandas())
-abund_data_norm = scale(abund_data)
-abund_data_use = abund_data
+abund_data_norm = np.array(normalize_table(cannon_data, use_cols)[use_cols].to_pandas())
+# OR for data standardization
+# abund_data_norm = scale(abund_data)
+abund_data_use = abund_data_norm
 
-move_to_dir('Results_limited')
+move_to_dir('Results_standardized')
 
 fig, ax = plt.subplots(1, 1)
 
-# print 'Perform PCA analysis'
-# pca = PCA(svd_solver='full', n_components=len(use_cols), copy=True)
-# pca.fit(abund_data_use)
-# visualize_components(pca.components_, use_cols, prefix='pca')
-# add_curve(calculate_explained_variance(abund_data_use, pca.transform(abund_data_use)), label='PCA', axes=ax)
-#
-# print 'Perform SDV analysis'
-# sdv = TruncatedSVD(n_components=len(use_cols)-1)
-# sdv.fit(abund_data_use)
-# add_curve(calculate_explained_variance(abund_data_use, sdv.transform(abund_data_use)), label='SDV', axes=ax)
-#
-# print 'Perform Factorial analysis'
-# fa = FactorAnalysis(n_components=len(use_cols), copy=True, svd_method='lapack')
-# fa.fit(abund_data_norm)
-# visualize_components(fa.components_, use_cols, prefix='fa')
-# add_curve(calculate_explained_variance(abund_data_norm, fa.transform(abund_data_norm)), label='Factor', axes=ax)
-#
-# print 'Perform FastICA analysis'
-# fast_ica = FastICA(n_components=len(use_cols))
-# fast_ica.fit(abund_data_use)
-# add_curve(calculate_explained_variance(abund_data_use, fast_ica.transform(abund_data_use)), label='Fast ICA', axes=ax)
+print 'Perform PCA analysis'
+pca = PCA(svd_solver='full', n_components=len(use_cols), copy=True)
+pca.fit(abund_data_use)
+visualize_components(pca.components_, use_cols, prefix='pca')
+add_curve(calculate_explained_variance(abund_data_use, pca.transform(abund_data_use)), label='PCA', axes=ax)
+
+print 'Perform SDV analysis'
+sdv = TruncatedSVD(n_components=len(use_cols)-1)
+sdv.fit(abund_data_use)
+add_curve(calculate_explained_variance(abund_data_use, sdv.transform(abund_data_use)), label='SDV', axes=ax)
+
+print 'Perform Factorial analysis'
+fa = FactorAnalysis(n_components=len(use_cols), copy=True, svd_method='lapack')
+fa.fit(abund_data_use)
+visualize_components(fa.components_, use_cols, prefix='fa')
+add_curve(calculate_explained_variance(abund_data_use, fa.transform(abund_data_use)), label='Factor', axes=ax)
+
+print 'Perform FastICA analysis'
+fast_ica = FastICA(n_components=len(use_cols))
+fast_ica.fit(abund_data_use)
+add_curve(calculate_explained_variance(abund_data_use, fast_ica.transform(abund_data_use)), label='Fast ICA', axes=ax)
 
 # print 'Perform NMF analysis'
 # min_val = np.nanmin(abund_data)
@@ -197,19 +199,27 @@ fig, ax = plt.subplots(1, 1)
 # !!! High usage of memory is expected.
 #
 
-n_rand = 5000
-idx_random = np.random.randint(0, len(abund_data), n_rand)
-abund_data_use = abund_data[idx_random, :]
-
-print 'Perform SparsePCA analysis'
-sparse_pca = SparsePCA(n_components=len(use_cols), n_jobs=4)
-sparse_pca.fit(abund_data_use)
-add_curve(calculate_explained_variance(abund_data_use, sparse_pca.transform(abund_data_use)), label='SparsePCA', axes=ax)
-
-print 'Perform KernelPCA analysis'
-kernel_pca = KernelPCA(n_components=len(use_cols), n_jobs=4, copy_X=True, kernel='poly')
-kernel_pca.fit(abund_data_use)
-add_curve(calculate_explained_variance(abund_data_use, kernel_pca.transform(abund_data_use)), label='KernelPCA', axes=ax)
+# n_rand = 30000
+# cpu_jobs = 2
+# idx_random = np.random.randint(0, len(abund_data), n_rand)
+# abund_data_use = abund_data[idx_random, :]
+#
+# # print 'Perform SparsePCA analysis'
+# # sparse_pca = SparsePCA(n_components=len(use_cols), n_jobs=4)
+# # sparse_pca.fit(abund_data_use)
+# # add_curve(calculate_explained_variance(abund_data_use, sparse_pca.transform(abund_data_use)), label='SparsePCA', axes=ax)
+#
+# print 'Perform KernelPCA analysis'
+# kernel_pca = KernelPCA(n_components=len(use_cols), n_jobs=cpu_jobs, copy_X=True, kernel='poly').fit(abund_data_use)
+# add_curve(calculate_explained_variance(abund_data_use, kernel_pca.transform(abund_data_use)), label='KernelPCA - poly', axes=ax)
+# kernel_pca = KernelPCA(n_components=len(use_cols), n_jobs=cpu_jobs, copy_X=True, kernel='linear').fit(abund_data_use)
+# add_curve(calculate_explained_variance(abund_data_use, kernel_pca.transform(abund_data_use)), label='KernelPCA - lin', axes=ax)
+# kernel_pca = KernelPCA(n_components=len(use_cols), n_jobs=cpu_jobs, copy_X=True, kernel='sigmoid').fit(abund_data_use)
+# add_curve(calculate_explained_variance(abund_data_use, kernel_pca.transform(abund_data_use)), label='KernelPCA - sigmoid', axes=ax)
+# kernel_pca = KernelPCA(n_components=len(use_cols), n_jobs=cpu_jobs, copy_X=True, kernel='cosine').fit(abund_data_use)
+# add_curve(calculate_explained_variance(abund_data_use, kernel_pca.transform(abund_data_use)), label='KernelPCA - cosine', axes=ax)
+# kernel_pca = KernelPCA(n_components=len(use_cols), n_jobs=cpu_jobs, copy_X=True, kernel='rbf').fit(abund_data_use)
+# add_curve(calculate_explained_variance(abund_data_use, kernel_pca.transform(abund_data_use)), label='KernelPCA - rbf', axes=ax)
 
 ax.set(xlabel='Number of components', ylabel='Variance percentage', xlim=[0, len(use_cols)], ylim=[0,105])
 ax.legend(loc='center right', bbox_to_anchor=(1.3, 0.5))
